410405066c8c4d01b903893ed5ea98d59e8ac3bf
martinbagshaw <martinbagshaw1@gmail.com>
2023-08-24 00:26:55 +0100

    use localStorage to save todos, apply subscribe method

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -14,6 +14,18 @@
     todoStore.removeTodo(todo);
   };
 
+  const handleLoad = () => {
+    todoStore.getSavedTodos();
+  };
+
+  const handleSave = () => {
+    todoStore.saveTodos();
+  };
+
+  const handleDelete = () => {
+    todoStore.deleteSavedTodos();
+  };
+
   // Local variables
   let currentTodo = "";
 </script>
@@ -29,6 +41,21 @@
         {$todoStore.showList ? "Hide" : "Show"} Todos
       </button>
       <!-- <button on:click={todoStore.toggleList}> Save todos </button> -->
+    {/if}
+
+    <!-- Load -->
+    {#if !$todoStore.todos.length && $todoStore.hasSavedTodos()}
+      <button on:click={handleLoad}> Load saved todos </button>
+    {/if}
+
+    <!-- Save -->
+    {#if $todoStore.todos.length}
+      <button on:click={handleSave}> {$todoStore.hasModifiedTodos() ? "Overwrite saved" : "Save"} todos </button>
+    {/if}
+
+    <!-- Delete -->
+    {#if $todoStore.todos.length && $todoStore.hasSavedTodos()}
+      <button on:click={handleDelete}> Delete saved todos </button>
     {/if}
 
     <form class="todo-form" on:submit|preventDefault={handleAddTodo}>
@@ -49,6 +76,6 @@
   </section>
 </div>
 
-<style type="text/scss">
+<style lang="scss">
   @import "./todos.scss";
 </style>
